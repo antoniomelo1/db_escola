980a31a8fec95eefa00d89bea58b932d81668218
Antonio Melo <antoniorodriguesmelo200@gmail.com>
2024-01-14 20:07:32 -0300

    Adicionado uma coluna endereço na tabela aluno e refatoração do código

--- db_escola/CREATE TABLE sessao (.sql
+++ db_escola/CREATE TABLE sessao (.sql
@@ -1,31 +1,62 @@
-CREATE TABLE sessao (
-  Codigo INT PRIMARY KEY,
-  Descricao VARCHAR(255) NOT NULL,
-  Localizacao VARCHAR(255) NOT NULL
-);
+CREATE TABLE `aluno` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `matricula` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE livro (
-  Cod_Livro INT PRIMARY KEY,
-  Titulo VARCHAR(255) NOT NULL,
-  Autor VARCHAR(255) NOT NULL,
-  Cod_Sessao INT NOT NULL,
-  FOREIGN KEY (Cod_Sessao) REFERENCES sessao (Codigo)
-);
 
-CREATE TABLE aluno (
-  id INT PRIMARY KEY,
-  nome VARCHAR(255) NOT NULL,
-  matricula INT NOT NULL UNIQUE,
-  email VARCHAR(255) NOT NULL,
-  telefone VARCHAR(20)
-);
+CREATE TABLE `emprestimo` (
+  `codigo` int(11) NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `matric_aluno` int(11) NOT NULL,
+  `cod_livro` int(11) NOT NULL,
+  `data_devolucao` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE emprestimo (
-  codigo INT PRIMARY KEY,
-  data_hora DATETIME NOT NULL,
-  matric_aluno INT NOT NULL,
-  cod_livro INT NOT NULL,
-  data_devolucao DATE,
-  FOREIGN KEY (matric_aluno) REFERENCES aluno (matricula),
-  FOREIGN KEY (cod_livro) REFERENCES livro (Cod_Livro)
-);
+
+CREATE TABLE `livro` (
+  `Cod_Livro` int(11) NOT NULL,
+  `Titulo` varchar(255) NOT NULL,
+  `Autor` varchar(255) NOT NULL,
+  `Cod_Sessao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `sessao` (
+  `Codigo` int(11) NOT NULL,
+  `Descricao` varchar(255) NOT NULL,
+  `Localizacao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `matricula` (`matricula`);
+
+
+ALTER TABLE `emprestimo`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `matric_aluno` (`matric_aluno`),
+  ADD KEY `cod_livro` (`cod_livro`);
+
+
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`Cod_Livro`),
+  ADD KEY `Cod_Sessao` (`Cod_Sessao`);
+
+
+ALTER TABLE `sessao`
+  ADD PRIMARY KEY (`Codigo`);
+
+
+ALTER TABLE `emprestimo`
+  ADD CONSTRAINT `emprestimo_ibfk_1` FOREIGN KEY (`matric_aluno`) REFERENCES `aluno` (`matricula`),
+  ADD CONSTRAINT `emprestimo_ibfk_2` FOREIGN KEY (`cod_livro`) REFERENCES `livro` (`Cod_Livro`);
+
+
+ALTER TABLE `livro`
+  ADD CONSTRAINT `livro_ibfk_1` FOREIGN KEY (`Cod_Sessao`) REFERENCES `sessao` (`Codigo`);
+COMMIT;
